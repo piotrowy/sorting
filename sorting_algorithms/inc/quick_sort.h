7f28eb3d706540310ce92408f34cd4adaec3052b
piotrowy <petr.ceranek@gmail.com>
2016-12-03 14:58:45 +0100

    cmake repaired

--- sorting_algorithms/inc/quick_sort.h
+++ sorting_algorithms/inc/quick_sort.h
@@ -5,4 +5,8 @@ void quickSort(int method, int guaranteeComplexity, int p, int r, int *table);
 
 void quickSortParametrized(int *table, int size);
 
+void quickSortIterative();
+
+void quickSortIterativeParametrized();
+
 #endif //SORTING_QUICK_SORT_H
